95e78a2147e10cbb3555dfd2ea626a2c689bd79c
Ananya Bhartiya <ananyabhartiya905@gmail.com>
2026-06-12 14:20:22 +0530

    my streamlit pokemon app

---
+++ 4.py
@@ -0,0 +1,8 @@
+#conditional statement( if else)
+number = int(input("Enter your number"))
+if number == 0:
+    print("Zero")
+elif number % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
---
+++ 5.py
@@ -0,0 +1,11 @@
+for x in range(1,11):
+    print(x)
+
+for x in range(1,20,2):
+    print(x)
+
+for x in range(5):
+    print("meow")
+
+for x in range(10,0,-1):
+    print(x)
