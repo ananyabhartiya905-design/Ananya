c06c79dc96ba9f357762618659dc05e198eb8ef4
Ananya Bhartiya <ananyabhartiya905@gmail.com>
2026-06-19 12:28:03 +0530

    streamlit ui

--- gemini.py
+++ gemini.py
@@ -1,23 +1,22 @@
-import google.genai as genai
 import streamlit as st
+import google.genai as genai
 
 API_KEY = st.secrets["GEMINI_API_KEY"]
 
 client = genai.Client(api_key=API_KEY)
 
-def get_fun_fact(topic: str):
+st.title("🤖 Gemini Fun Fact Bot")
+
+topic = st.text_input("Enter a topic:", "RC fighter jets")
+
+if st.button("Get Fun Fact"):
     try:
         response = client.models.generate_content(
             model="gemini-2.5-flash",
             contents=f"Tell me a fun fact about {topic}. Keep it short and simple."
         )
-        return response.text
+
+        st.success(response.text)
 
     except Exception as e:
-        return f"Error: {e}"
-
-
-if __name__ == "__main__":
-    topic = "RC fighter jets"
-    fact = get_fun_fact(topic)
-    print("🤖 Fun Fact:\n", fact)
+        st.error(f"Error: {e}")
